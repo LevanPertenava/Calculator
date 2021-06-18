8ae9ac528efecb8c78d1a3a3c2fe6b020c5468ba
LevanPertenava <levan.pertenava1@gmail.com>
2021-06-18 16:44:34 +0400

    update

--- script.js
+++ script.js
@@ -1,25 +1,49 @@
 $(document).ready(function(){
     var btnValue = "";
     var inputValue = "";
-    
+    var isEqualled = false;
+
     $(".button").click(function(){        
         btnValue = $(this).val();
-
-        if(inputValue.length == 0 && btnValue == 0){
+/*         try{
+        if(inputValue.includes(".") && btnValue == "."){
+            return;
+        }}
+        catch(e){
+            alert(e.message);
+        } */
+         if(inputValue.length == 0 && btnValue == 0){
             return;
         }
-        if(btnValue == "c"){
-            inputValue = "";
-            btnValue = "";
-            $("#result").val(0);
-            return;
-        }
-        if(btnValue == "="){
-            inputValue = eval(inputValue);
+        if(inputValue.length == 0 && isNaN(btnValue) && btnValue != "c"){
+            inputValue = "0";
+            inputValue += btnValue;
             $("#result").val(inputValue);
             return;
         }
-        inputValue += btnValue;
+        if(btnValue == "c"){
+            reset();
+            $("#result").val(0);
+            return;
+        } 
+        if(btnValue == "="){
+            inputValue = eval(inputValue);
+            $("#result").val(inputValue);
+            isEqualled = true;
+            return;
+        }
+         if(isEqualled == true && !isNaN(btnValue)){                      
+            reset();   
+            $("#result").val(inputValue = btnValue); 
+            return;
+        }           
+        isEqualled = false;
+        inputValue += btnValue;       
         $("#result").val(inputValue);
     });
+
+    function reset(){
+        inputValue ="";
+        isEqualled = false; 
+    }
 });
