36825db9717a3b0379b5b39ac47645bff03f8c3f
LevanPertenava <levan.pertenava1@gmail.com>
2021-06-21 15:58:55 +0400

    with value exchanges

--- script.js
+++ script.js
@@ -2,16 +2,11 @@ $(document).ready(function(){
     var btnValue = "";
     var inputValue = "";
     var isEqualled = false;
+    let selectedCurrency= "";
+    let array = [];
 
     $(".button").click(function(){        
         btnValue = $(this).val();
-/*         try{
-        if(inputValue.includes(".") && btnValue == "."){
-            return;
-        }}
-        catch(e){
-            alert(e.message);
-        } */
          if(inputValue.length == 0 && btnValue == 0){
             return;
         }
@@ -45,5 +40,63 @@ $(document).ready(function(){
     function reset(){
         inputValue ="";
         isEqualled = false; 
+    }
+
+    $.ajax({
+        method: "GET",
+        dataType: "json",
+        url: "currency.json",
+        success: function(currencies){
+            $(currencies).each(function(i, currency){      
+                $("#currencies").append('<li ><a class="dropdown-item currencies" href="#">'+ currency +'</a></li>');  
+                });
+            }
+        });        
+    
+    $(".dropdown").on("click",".currencies",function(){
+        var currency = $(this).text();
+        if(selectedCurrency == currency){ 
+            return;
+        }
+        selectedCurrency= currency;  
+        $("#result").val(0);
+        $("#dropdownMenuButton2").text("Date");
+        $("#dropdownMenuButton2").attr("disabled",false);
+        $("#dropdownMenuButton1").text(currency);
+
+        priceHistory(currency);        
+    });
+//meore nawili json fasebi da tarigebi
+
+    $(".dropdown").on("click", ".dates", function(){
+        var selectedDate = $(this).text();
+        $("#dropdownMenuButton2").text(selectedDate);
+        for (let i = 0; i < array.length; i++) {
+            let date = new Date(Object.keys(array[i]))
+            if(date.toDateString() == selectedDate){
+            $("#result").val(inputValue = Object.values(array[i]));
+            }
+        } 
+    });
+    function priceHistory(currency){
+            $.ajax({
+                method: "GET",
+                dataType: "json",
+                url: "info.json",
+                success: function(result){
+                array = result[currency]
+                let date;
+                let count = 1;
+                for(let i = array.length - 1; i > array.length - 8; i--){
+                    date = new Date(Object.keys(array[i]));  
+                    if($("#dropitem"+ count +"").length == 0) {
+                        $("#dates").append('<li ><a class="dropdown-item dates" href="#" id="dropitem'+ count++ +'">'+ date.toDateString() +'</a></li>');          
+                    }
+                    else{
+                        $("#dropitem"+ count++ +"").text(date.toDateString());      
+                    }
+                }                 
+            }
+        });
     }
 });
